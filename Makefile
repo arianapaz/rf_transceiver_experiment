831796951719854f89f9620e43e5069f8c04ad50
Cameron Bost <cameron.a.bost@gmail.com>
2019-11-30 14:32:40 -0600

    Stratify Exp3 Car/Key into 2 files

--- Makefile
+++ Makefile
@@ -10,12 +10,15 @@ experiment2: ./lib/rc-switch/RCSwitch.o ./lib/RFContext.o experiment2.o
 experiment2_2: ./lib/rc-switch/RCSwitch.o ./lib/RFContext.o experiment2_2.o
 	$(CXX) $(CXXFLAGS) $(LDFLAGS) $+ -o $@ -lwiringPi -lwiringPiDev -lcrypt
 
-experiment3: ./lib/rc-switch/RCSwitch.o ./lib/RFContext.o experiment3.o
+experiment3_car: ./lib/rc-switch/RCSwitch.o ./lib/RFContext.o experiment3_car.o
+	$(CXX) $(CXXFLAGS) $(LDFLAGS) $+ -o $@ -lwiringPi -lwiringPiDev -lcrypt
+
+experiment3_key: ./lib/rc-switch/RCSwitch.o ./lib/RFContext.o experiment3_key.o
 	$(CXX) $(CXXFLAGS) $(LDFLAGS) $+ -o $@ -lwiringPi -lwiringPiDev -lcrypt
 
 experiment3_THIEF: ./lib/rc-switch/RCSwitch.o ./lib/RFContext.o experiment3_THIEF.o
 	$(CXX) $(CXXFLAGS) $(LDFLAGS) $+ -o $@ -lwiringPi -lwiringPiDev -lcrypt
 
 clean:
-	$(RM) ./lib/rc-switch/*.o *.o servo experiment2 experiment2_2 experiment3 experiment3_THIEF
+	$(RM) ./lib/rc-switch/*.o *.o servo experiment2 experiment2_2 experiment3_car experiment3_key experiment3_THIEF
 
---
+++ experiment3_key.cpp
@@ -0,0 +1,115 @@
+#include "lib/RFContext.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+const long SEED = 425;
+const long RAND_A = 153;
+const long RAND_B = 11;
+const long RAND_M = 8388608; // 2^23
+
+
+const int CHALLENGE_INTERVAL = 10000*3;
+
+vector<int> codeCache;
+const int CACHE_SIZE = 250;
+int currentChallengeMessage = 0, currentRollingCode = 0;
+
+/**
+ * Linear Congruential Generator to generate random numbers
+ */
+long random(long s)
+{
+	return ((RAND_A * s)%RAND_M + RAND_B) % RAND_M;
+}
+
+/**
+ * Setup the code cache by adding CACHE_SIZE codes to accepted set.
+ */
+void setupQueue(long rnd){
+	currentChallengeMessage = random(rnd);
+	codeCache.push_back(random(currentChallengeMessage));
+	currentRollingCode = codeCache.front();
+	for(int i = 0; i < CACHE_SIZE; i++){
+		codeCache.push_back(random(codeCache.back()));
+	}
+}
+
+/**
+ * Update codes that car is expecting by removing
+ * from cache then shifting one more index to the right.
+ * 
+ * @param value code that was received
+ */
+void shiftCode(int value){
+	int numShifted = 0;
+	currentRollingCode = codeCache.front();
+	while (currentRollingCode != value) {
+		codeCache.erase(codeCache.begin());
+		codeCache.push_back(random(codeCache.back()));
+		currentRollingCode = codeCache.front();
+		numShifted++;
+	}
+	numShifted++;
+	printf("cache shifted by %i codes\n", numShifted);
+}
+
+/**
+ * Insertion point
+ * 
+ * @param argc number of CLI arguments
+ * @param argv vector of CLI arguments
+ */
+int main(int argc, char *argv[]){
+	RFContext ctx = RFContext();
+	
+	long rnd = random(SEED);
+	setupQueue(rnd);
+	
+	// Run forever
+	unsigned long iterCount = 0;
+	bool first = true;
+	while(1){
+	  // Introduction
+	  if(first){
+		printf("HELLO I AM KEY\nWaiting for code %i\n", currentChallengeMessage);
+		first = false;
+	  }
+	  // If message there
+	  if(ctx.available()){
+		int value = ctx.getReceivedValue();
+		
+		if (value == 0) {
+		  printf("ERR: Unknown encoding\n");
+		}
+		else{
+			printf("Received message %i\n", value);
+			// Received valid challenge message
+			bool codeIsInCache = find(codeCache.begin(), codeCache.end(), value) != codeCache.end();
+			if(value == currentChallengeMessage || codeIsInCache){
+				printf("Challenge message received (%i).\n", value);
+				if(codeIsInCache){
+					printf("Updating cache...");
+					shiftCode(value);
+					currentChallengeMessage = value;
+					printf("New challenge message: %i\n", currentChallengeMessage);
+				}
+				// Get next rolling code
+				codeCache.erase(codeCache.begin());
+				codeCache.push_back(random(codeCache.back()));
+				currentRollingCode = codeCache.front();
+				printf("Sending rolling code (%i)...\n", currentRollingCode);
+				ctx.send_code(currentRollingCode);
+			}
+		}
+		fflush(stdout);
+	  }
+	  ctx.resetAvailable();
+      usleep(100);	// Sleep for 0.0001 seconds
+      iterCount++;
+	}
+	
+	return 0;
+}
